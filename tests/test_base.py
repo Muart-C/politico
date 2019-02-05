1d40d54efc53b3332a60eaec22b01d15c84a7593
Muart-C <charles.muathe@strathmore.edu>
2019-02-05 16:22:16 +0300

    [starts #163687913] add get parties route

--- tests/test_base.py
+++ tests/test_base.py
@@ -10,32 +10,63 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         app = create_app()
         self.client = app.test_client()
+        
+        #a dictionary containing a party
         self.party = {
                 "Id" : "1",
                 "name" : "ANC",
                 "hqAddress" : "Kakamega",
                 "logoUrl" : "https://goo.gl/images/B9U4PK",
             }
+        
+        #a list of containing dictionaries containing parties
+        self.parties = [
+            {
+                "Id" : "1",
+                "name" : "ANC",
+                "hqAddress" : "Kakamega",
+                "logoUrl" : "https://goo.gl/images/B9U4PK",
+            },
+            {
+                "Id" : "2",
+                "name" : "Jubilee",
+                "hqAddress" : "Muthaiga",
+                "logoUrl" : "https://goo.gl/images/7hU72H",
+            },
+            {
+                "Id" : "3",
+                "name" : "Maendeleo Chapchap",
+                "hqAddress" : "Machakos",
+                "logoUrl" : "https://goo.gl/images/3RKgQ6",
+            }
+        ]
 
     def test_add_party(self):
         """ensure a new party can be added to the political party list."""
         with self.client:
             response = self.client.post(
                 '/api/v1/parties',
-                data = json.dumps(
-                    {
-                        "Id" : "1",
-                        "name" : "ANC",
-                        "hqAddress" : "Kakamega",
-                        "logoUrl" : "https://goo.gl/images/B9U4PK",
-                    }
-                ),
+                data = json.dumps(self.party),
                 content_type = "application/json",
             )
             data = json.loads(response.data)
             self.assertEqual(response.status_code, 201)
             self.assertIn('ANC was added', data['success'])
-            
+
+    def get_parties(self):
+        """a list of political parties is returned."""
+        with self.client:
+            response = self.client.get(
+                '/api/v1/parties',
+                data = json.dumps(self.parties),
+                content_type = "application/json",
+            )
+            data = json.loads(response.data)
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(data['data'], list)
+        
+    
+    
 
 
 
