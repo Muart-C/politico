a70eb6e8568ae1dabd52cfc6d458b9c0bb953052
Muart-C <charles.muathe@strathmore.edu>
2019-02-05 19:28:56 +0300

    [finishes #163687913] add get list of political parties endpoint

--- api/admin/party/model.py
+++ api/admin/party/model.py
@@ -1,19 +1,32 @@
 #party class for data structure model
-
-#instatiate an empty list of political parties
-political_parties = []
-
+parties = []
 class Party(object):
     """Party model to store party data in data structures"""
-    def __init__(self, Id, name, hqAddress, logoUrl):
-        self.Id = Id
-        self.name = name
-        self.hqAddress = hqAddress
-        self.logoUrl = logoUrl
+    def __init__(self):
+        """#instatiate an empty list of political parties."""
+        self.political_parties = parties
 
     #create a party.
-    def add_party(self):
-        political_parties.append(self)
+    def add_party(self, name, hqAddress, logoUrl):
+        party = {
+            "Id" : len(self.political_parties)+1,
+            "name" : name,
+            "hqAddress" : hqAddress,
+            "logoUrl" : logoUrl,
+        }
+        self.political_parties.append(party)
+        return party
+    
+    #get all parties
+    def get_parties(self):
+        return self.political_parties
+
+    #get one political party
+    def get_party(self, Id):
+        if self.political_parties:
+            for party in self.political_parties:
+                if party.get("Id") == Id:
+                    return party
 
 
     
