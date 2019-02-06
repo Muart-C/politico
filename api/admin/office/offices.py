7654849e7b10fa33abe613f274bd60c8e7e86062
Muart-C <charles.muathe@strathmore.edu>
2019-02-06 09:17:14 +0300

    [finishes #163688220] update get a government office endpoint

--- api/admin/office/offices.py
+++ api/admin/office/offices.py
@@ -1,75 +1,76 @@
 from flask import Blueprint, jsonify, request
 from api.admin.office.model import Office
 
-#initialize a office blueprint
+#initialize an office blueprint
 office_blueprint = Blueprint('offices', __name__)
 
-#create a office route
+#create post office route
 @office_blueprint.route('/offices', methods=['POST'])
 def add_offices():
     try:
         data = request.get_json()
         name = data.get('name')
         office_type = data.get('office_type')
-    #returns an error response incase the body of the post request is empty
+    #returns an error response incase of an error
     except:
         return jsonify({
             "status" : 400,
-            "error" : "invalid request payload"
+            "error" : "invalid request"
         })
     
-    #return a json response with the data payload of the details
+    #return a json response of the office created
     response = Office().add_office(name, office_type)
     return jsonify({
         "status" : 201,
         "data" : response,
-        "success" : "office {} was added".format(name)
+        "success" : "the office {} was added".format(name)
     }), 201
 
 #get all offices route    
 @office_blueprint.route('/offices', methods=['GET'])
 def get_offices():
-    """get all political offices"""
+    """get all the offices"""
 
-    #initialize the office model
+    #initialize an office data model
     offices = Office()
 
-    #get a list of all political offices
+    #get a list of all  offices
     political_offices = offices.get_offices()
 
-    #checks if a list of political offices exists then returns it as a json response
+    #checks if a list of political offices exists then returns it
     if political_offices:
         political_offices_response = {
             "status" : 200,
             "data" : political_offices,
-            "success" : "get request to get list of offices was successful",
+            "success" : "get list of offices was successful",
         }
         return jsonify(political_offices_response), 200
 
 
-    #incase the request is unsuccessful json error response is returned
+    #if unsuccessful an error is returned
     return jsonify({
         "status": 404, 
-        "error": "get request to get list of offices was was unsuccessful"
+        "error": "request was unsuccessful"
         }), 404 
     
-#get a particular office route
+#get a particular office route endpoint
 @office_blueprint.route('/offices/<int:Id>', methods=['GET'])
 def get_office(Id):
-    """get a particular office."""
+    """get a particular political office."""
     
-    #initialize a office model
+    #initialize an office data structure
     political_office = Office()
 
-    #get a office
+    #get an office with the id passed
     office = political_office.get_office(Id)
 
-    #checks if the office exists then returns the office as a json response
+    #if the office exists then return it as json response
     if office:
         return jsonify({
             "status" : 200,
             "data" : [office],
-            "success" : "request was successful and a result was returned",
+            "success" : "request was successful",
         })
-    #incase the request is unsuccessful json error response is returned
+
+    #return error response
     return jsonify({"status": 404, "error": "no office with that id was found"}), 404 
