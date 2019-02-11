7ccdece7d1ee65837602791ad444170365ba4ce7
Muart-C <charles.muathe@strathmore.edu>
2019-02-11 23:43:24 +0300

    [bg #163874686] add validation functions for create party

--- api/admin/party/parties.py
+++ api/admin/party/parties.py
@@ -1,7 +1,10 @@
 """!#api/admin/party"""
+import validators
 from flask import Blueprint, request
 from api.models.parties_model import Party, PARTIES
-from api.utils.validator import return_response,validate_string_data_type, validate_int_data_type
+from api.utils.validator import return_response, validate_string_data_type, check_json_party_keys
+from api.utils.validator import return_error, validate_int_data_type, sanitize_input
+from api.utils.validator import check_is_valid_url
 
 #initialize a party blueprint
 PARTY_BLUEPRINT = Blueprint('parties', __name__)
@@ -9,23 +12,30 @@ PARTY_BLUEPRINT = Blueprint('parties', __name__)
 #create a party route
 @PARTY_BLUEPRINT.route('/parties', methods=['POST'])
 def add_parties():
-    data = request.get_json()
+    """add a new party if it does not exist."""
 
-    if not data:
-        return return_response(400, "fill in all the required values")
+    key_errors=check_json_party_keys(request)
+    if key_errors:
+        return return_error(400, "Invalid keys provided")
     try:
+        data = request.get_json()
         # validate data from the request
         name=data['name']
         hqAddress=data['hqAddress']
         logoUrl=data['logoUrl']
 
-        if(validate_string_data_type(name) == False):
-            return return_response(400, "the name cannot be empty")
+        # ensure keys data values of correct format
+        if(validate_string_data_type(name) == False) and name:
+            return return_error(400, "the name should be of correct data type")
         if(validate_string_data_type(hqAddress) == False):
-            return return_response(400, "the HQ cannot be empty")
-        if(validate_string_data_type(logoUrl) == False):
-            return return_response(400, "the Logo is needed add the url")
+            return return_error(400, "the HQ be of correct data type")
+        if(check_is_valid_url(logoUrl) == False):
+            return return_error(400, "the Logo url is not in the correct format")
 
+        if(sanitize_input(name)) == False:
+            return return_error(400, "name is in the wrong format")
+        if (sanitize_input(hqAddress)) == False:
+            return return_error(400, "hq address is in the wrong format")
     except KeyError as e:
         return return_response(400, "an error occurred while creating party {} is missing".format(e.args[0]))
 
@@ -34,7 +44,7 @@ def add_parties():
     if party:
         return return_response(201, "party {} was created".format(name), [party])
     #if not success return error
-    return return_response(400, "an error occurred while creating party")
+    return return_error(400, "an error occurred while creating party")
 
 #get all parties route
 @PARTY_BLUEPRINT.route('/parties', methods=['GET'])
@@ -49,7 +59,7 @@ def get_parties():
     if political_parties:
         return return_response(200, "get request is for parties successful", political_parties)
     #incase the request is unsuccessful json error response is returned
-    return return_response(400, "the party list was empty")
+    return return_error(400, "the party list was empty")
 
 #get a particular party route
 @PARTY_BLUEPRINT.route('/parties/<int:id>', methods=['GET'])
@@ -57,7 +67,7 @@ def get_party(id):
     """get a particular party."""
 
     if(validate_int_data_type(id) == False):
-        return return_response(400, "please provide id of correct type")
+        return return_error(400, "invalid url arguments provided")
 
     #initialize a party model
     political_party = Party()
@@ -70,7 +80,7 @@ def get_party(id):
         return return_response(200, "a party with the id was returned", party)
 
     #incase the request is unsuccessful json error response is returned
-    return return_response(404, "no party with that id was found")
+    return return_error(404, "no party with that id was found")
 
 #update the name route
 @PARTY_BLUEPRINT.route('/parties/<int:party_id>/name', methods=['PATCH'])
@@ -79,9 +89,9 @@ def change_name(party_id):
         data = request.get_json()
         name=data["name"]
         if(validate_string_data_type(name)== False):
-            return return_response(400, "the name should of string data type")
+            return return_error(400, "the name should of string data type")
     except KeyError as e:
-            return return_response(400, "an error the party {} is missing".format(e.args[0]))
+            return return_error(400, "an error the party {} is missing".format(e.args[0]))
 
     #initialize the Party model class
     political_party = Party()
@@ -117,4 +127,19 @@ def delete_party(party_id):
         return return_response(204, "party deleted")
     #if party not in list return error
     return return_response(404, "error processing your request")
+
+
+#update the details of the party
+@PARTY_BLUEPRINT.route('/parties/<int:party_id>', methods=['PATCH'])
+def edit_party(party_id):
+    """updates details of a particular party."""
+    if(validate_int_data_type(id) == False):
+        return return_error(400, "invalid url arguments provided")
+    
+    data = request.get_json()
+    party = Party()
+    # update
+    updated_party = party.update_party(party_id, data)
+
+
 
--- api/utils/validator.py
+++ api/utils/validator.py
@@ -1,6 +1,11 @@
 """validation functions"""
+import re
 from flask import jsonify, make_response
 
+
+def sanitize_input(input_data):
+    if input_data.isalpha() == False:
+        return False
 
 def validate_int_data_type(data_passed):
     """ ensures the inputs are of int  type"""
@@ -18,6 +23,15 @@ def generate_id(list_of_items):
     """generates the id of a new item given a list"""
     return len(list_of_items) +1
 
+def return_error(status_code, message):
+    """ function to format the response """
+    response = {
+        "status": status_code,
+        "error": message,
+    }
+    return make_response(jsonify(response), status_code)
+
+
 def return_response(status_code, message, data=list()):
     """ function to format the response """
     response = {
@@ -26,6 +40,24 @@ def return_response(status_code, message, data=list()):
         "data": data,
     }
     return make_response(jsonify(response), status_code)
+
+
+def check_json_party_keys(request):
+    """checks if keys of the payload are correct"""
+    request_keys = ["name", "hqAddress", "logoUrl"]
+    errors = []
+    for key in request_keys:
+        if not key in request.json:
+            errors.append(key)
+        return errors
+
+
+def check_is_valid_url(url):
+    """check if the url provided is valid"""
+    if re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
+               url):
+       return True
+    return False
 
 # def checks_if_exist(key, value, collection_of_items):
 #     """check if the value of the key attribute passed exists."""
