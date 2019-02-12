bdc60546b8d029820f08d9a79ee44229d6444cc5
Muart-C <charles.muathe@strathmore.edu>
2019-02-12 20:16:06 +0300

    [chore #163910557] add a database class and setup environment configurations

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+"""define app configurations"""
+import os
 
 class Config:
     """base class for configurations for all phases"""
@@ -8,6 +10,7 @@ class TestingConfiguration(Config):
     """configuration for use during testing phase."""
     TESTING = True
     DEBUG = True
+    DATABASE_URL_TEST = os.getenv("DATABASE_TEST_URL")
 
 class DevelopmentConfiguration(Config):
     """configurations for use during development phase."""
@@ -23,3 +26,4 @@ app_config = {
     'production' : ProductionConfiguration,
 }
 
+database_url_test = TestingConfiguration.DATABASE_URL_TEST
