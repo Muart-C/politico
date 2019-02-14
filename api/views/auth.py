f526e3d67ffbb21e2cc20b11df071b08c006b762
Muart-C <charles.muathe@strathmore.edu>
2019-02-14 17:31:49 +0300

    [starts #163900280] add login endpoint

--- api/views/auth.py
+++ api/views/auth.py
@@ -1,9 +1,12 @@
 """authentication endpoints"""
 import re
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response, jsonify
+from werkzeug.security import check_password_hash
 from api.utils.validator import check_json_new_user_keys,\
-     return_error, return_response, validate_string_data_type,\
-         sanitize_input, check_email_validity, check_is_valid_url
+     return_error, validate_string_data_type,\
+         sanitize_input, check_email_validity, check_is_valid_url\
+
+from api.utils.validator import validate_password, encode_auth_token
 from api.models.users_model import User
 
 # add auth blueprint
@@ -51,9 +54,9 @@ def create_user():
             return return_error(400, "provide a valid last name")
         if(sanitize_input(othername) == False):
             return return_error(400, "provide a valid other name")
-
-        if re.match('[a-zA-Z0-9@#$&^%+=-]{6,}', password) is None:
-            return return_error(400, "password must be 6 or more characters")
+        if(validate_password(password) == False):
+            return return_error(400,\
+                 "password should be more than six characters")
 
     except KeyError as e:
         return return_error(400, "an error occurred while creating user  {} is missing".format(e.args[0]))
@@ -61,12 +64,46 @@ def create_user():
     user = User(email=email, password=password, firstname=firstname, \
         lastname=lastname, othername=othername, phone_number=phone_number, passport_url=passport_url)
 
+    if user.create_user():
+        return make_response(jsonify({
+            "status":201,
+            "message": "user successfully created",
+            "data" :[{
+                "email": user.email,
+                "firstname": user.firstname,
+            }]
+        }))
+    return return_error(400, "user already exists")
 
-    if user.check_if_user_exist_before_creating_one() is True:
-        return return_error(400, "user already exists")
-    new = user.create_user()
-    if new:
-        return return_response(201, "user was created successfully")
-    return return_error(500, "error creating a user")
+# login
+@AUTH_BLUEPRINT.route('/auth/login', methods=["POST"])
+def login():
+    """login as a user."""
+    try:
+        data = request.get_json()
+        email = data['email']
+        password = data['password']
 
+        if(check_email_validity(email) is False):
+            return return_error(400, "add a valid email address")
+        if(validate_password(password) is False):
+            return return_error(400,\
+                 "password should be more than six characters")
 
+        user = User(email=email, )
+        user = user.get_user(email)
+        if user:
+            return return_error(400, "user does not exist")
+        password = check_password_hash(user.password, password)
+        if password:
+            """generate token for user"""
+            token = encode_auth_token(user.email)
+            if token:
+                return make_response(jsonify({
+                    "status":200,
+                    "message":"successful login",
+                    "token": token.decode("utf-8")
+                }))
+    except Exception as exception:
+        print(exception)
+        return return_error(400, "an error occurred while login in")
