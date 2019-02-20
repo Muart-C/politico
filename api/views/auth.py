1ec621b29596e14d660ee62b94955411e031bb2d
Muart-C <charles.muathe@strathmore.edu>
2019-02-20 12:02:32 +0300

    [Finishes #163900280] Edit response messages to be sentence case

--- api/views/auth.py
+++ api/views/auth.py
@@ -27,32 +27,32 @@ def create_user():
         password=data['password']
 
         if(check_email_validity(email) is False):
-            return return_error(400, "enter the correct email format i.e should contain @ and .")
+            return return_error(400, "Enter the correct email format i.e should contain @ and .")
         if(validate_string_data_type(firstname) is False):
-            return return_error(400, "the first name should contain character of words")
+            return return_error(400, "The first name should contain character of words")
         if(validate_string_data_type(lastname) is False):
-            return return_error(400, "the last name should contain character of words")
+            return return_error(400, "The last name should contain character of words")
         if(validate_string_data_type(othername) is False):
-            return return_error(400, "the other name should contain character of words")
+            return return_error(400, "The other name should contain character of words")
         if(check_email_validity(email) is False):
-            return return_error(400, "enter the correct email format")
+            return return_error(400, "Enter the correct email format")
         if(validate_string_data_type(phone_number) is False):
-            return return_error(400, "enter a correct phone number")
+            return return_error(400, "Enter a correct phone number")
         if(check_is_valid_url(passport_url) is False):
-            return return_error(400, "the passport url should be of correct format should be of format https://myimage.com")
+            return return_error(400, "The passport url should be of correct format should be of format https://myimage.com")
 
         if(sanitize_input(firstname) == False):
-            return return_error(400, "provide a valid first name it should not contain any spaces")
+            return return_error(400, "Provide a valid first name it should not contain any spaces")
         if(sanitize_input(lastname) == False):
-            return return_error(400, "provide a valid last name, it should not contain any spaces")
+            return return_error(400, "Provide a valid last name, it should not contain any spaces")
         if(sanitize_input(othername) == False):
-            return return_error(400, "provide a valid other name,it should not contain any spaces ")
+            return return_error(400, "Provide a valid other name,it should not contain any spaces ")
         if(validate_password(password) == False):
             return return_error(400,\
-                 "password should be more than six characters")
+                 "Password should be more than six characters")
 
     except KeyError as e:
-        return return_error(400, "an error occurred while creating user  {} is missing".format(e.args[0]))
+        return return_error(400, "An error occurred while creating user  {} is missing".format(e.args[0]))
     pass_hash = generate_password_hash(password)
     user = User(email=email, password=pass_hash, firstname=firstname,\
         lastname=lastname, othername=othername, phone_number=phone_number,\
@@ -69,7 +69,7 @@ def create_user():
                 }
             }]
         }),201)
-    return return_error(409, "the user you are trying to register already exists")
+    return return_error(409, "User already exists")
 
 # login
 @AUTH_BLUEPRINT.route('/auth/login', methods=["POST"])
@@ -79,9 +79,9 @@ def login():
     password = data['password']
 
     if(check_email_validity(email) is False):
-        return return_error(400, "add a valid email address")
+        return return_error(400, "Add a valid email address")
     if(validate_password(password) is False):
-        return return_error(400,"password should be more than six characters")
+        return return_error(400,"Password should be more than six characters")
 
     user = User(email=None, password=None, firstname=None,\
         lastname=None, othername=None, phone_number=None,\
@@ -89,7 +89,7 @@ def login():
     check_user = user.get_user(email=email)
     user = json.loads(check_user)
     if not user:
-        return return_error(404, "the user you are trying to get does not exist")
+        return return_error(404, "User is not found")
     check_password= check_password_hash(user[7],password)
     if check_password:
         token = encode_auth_token(email)
@@ -102,4 +102,4 @@ def login():
                     }],
             }), 200)
     else:
-        return return_error(404, "ensure you input valid email and password")
+        return return_error(401, "Ensure you input valid email and password")
