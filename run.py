ed1d3575e52c765ef2957ca4642ea0a9ade0dc6b
Muathe Ndirangu <charles.muathe@strathmore.edu>
2019-02-22 08:30:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ from api.database.database import DatabaseSetup
 
 environment=os.getenv('APP_SETTING', 'development')
 app = create_app(environment)
-
 if __name__ == "__main__":
+    python api_docs.py
     app.run()
