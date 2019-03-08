ae7c650a95bfd2d8fb6c25efdc1317e24280d530
Muart-C <charles.muathe@strathmore.edu>
2019-03-08 12:51:05 +0300

    [Finishes #164489308] Add reset password functionality

--- UI/js/password_reset.js
+++ UI/js/password_reset.js
@@ -2,33 +2,34 @@ const send_reset_url = 'https://api-politico-v2.herokuapp.com/api/v2/auth/reset'
 function requestPasswordReset() {
     let email = document.getElementById('email').value;
     console.log(email)
-    fetch(send_reset_url, {
+    let post_email = {
         method: 'POST',
-        headers: {
+        headers: new Headers({
             'Content-Type': 'application/json',
-        },
+        }),
         body: JSON.stringify({
             email: email
         }),
-    })
-    console.log(BASE_API_URL)
+    }
+    fetch(send_reset_url, post_email)
     .then(res => res.json())
     .then((data) => {
-        console.log(data.status)
-        // if (data.status === 200) {
-        //     showSuccessMessage(data['message']);
-        // }else {
-        //     showErrorMessage(data['error']);
-        // }
-
+        let message = data['message']
+        let error = data['error']
+     if (message) {
+         alert("Check your email for password reset link")
+     }else if(error){
+        alert("Your email does not exist register as new user")
+     }
     })
-    // .catch((error) => {
-    //     showErrorMessage('An error occurred while checking your email');
-    // });
+    .catch((error) => {
+        console.log(error)
+    });
 }
 
 
 function updatePassword() {
+    let email = document.getElementById('email').value
     let password = document.getElementById('password').value
     let confirmPassword = document.getElementById('confirm-password').value
     if (password !== confirmPassword){
@@ -37,12 +38,12 @@ function updatePassword() {
     }
     fetch(`${BASE_URL}/reset`, {
         method: 'POST',
-        headers: {
+        headers: new Headers({
             'Content-Type': 'application/json',
-            'authorization': `Bearer ${token}`
-        },
+        }),
         body: JSON.stringify({
-            password:password
+            password : password,
+            email : email
         }),
     })
     .then(res => res.json())
